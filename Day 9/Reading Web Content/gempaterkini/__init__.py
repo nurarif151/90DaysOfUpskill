c2a3d22e65617a4deeb307de3f6f92ced5c075b5
nurarif151 <nurarif0151@gmail.com>
2023-02-15 16:03:25 +0700

    Day 9

---
+++ Day 9/Reading Web Content/gempaterkini/__init__.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def ekstraksi_data():
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception:
+       return None
+
+    if content.status_code == 200:
+        print(content.text)
+        return content.text
+
+
+
+def tampilkan_data(result):
+    if result is None:
+        print("Sorry the data could not be displayed")
+    else :
+        print("Ok.... Done. All data is displayed successfully")
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     print('ini adalah package gempa terkini')
+#     print('hai')
